a961ed185c2703a83431e16e965f57ea503e71b7
phosah <efosa.uyi@gmail.com>
2021-01-08 13:22:29 +0100

    updated sidebar hrefs

--- public/sidebar.php
+++ public/sidebar.php
@@ -4,23 +4,23 @@
             <div class="w-3/4 m-auto mb-8"><img src="img/dashboard-logo.png" alt="Dashboard logo"></div>
             <div class="group flex mb-8 items-center w-3/4 m-auto">
                 <div class="mr-3 group-focus:text-blue-700"><img src="img/dashboard-icon.png" alt="Dashboard icon"></div>
-                <a href="dashboard.php" class="bg-white group-focus:text-blue-500">Dashboard</a>
+                <a href="dashboard.html" class="bg-white group-focus:text-blue-500">Dashboard</a>
             </div>
             <div class="flex <?=$wallet ?> items-center w-3/4 m-auto mb-8">
                 <div class="mr-3"><img src="img/wallet.png" alt="Wallet icon"> </div>
-                <a href="wallet.php">Wallet</a>
+                <a href="wallet.html">Wallet</a>
             </div>
             <div class="flex <?php echo $deposit ?> items-center mb-8 w-3/4 m-auto">
                 <div class="mr-3"><img src="img/deposit.png" alt="Deposit icon"></div>
-                <a href="deposit.php">Deposit</a>
+                <a href="deposit.html">Deposit</a>
             </div>
             <div class="flex items-center mb-8 w-3/4 m-auto">
                 <div class="mr-3"><img src="img/withdrawal.png" alt="Withdrawal icon"></div>
-                <a href="withdraw.php">Withdrawal</a>
+                <a href="withdraw.html">Withdrawal</a>
             </div>
             <div class="flex items-center mb-8 w-3/4 m-auto">
                 <div class="mr-3"><img src="img/profile.png" alt="Profile icon"></div>
-                <a class="" href="profile.php">Profile</a>
+                <a class="" href="profile.html">Profile</a>
             </div>
         </div>
         <div class="flex w-3/4 m-auto">
